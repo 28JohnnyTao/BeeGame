e346f2ed49ea26205ad3024c9b874cbbc37786e9
28JohnnyTao <77082294+28JohnnyTao@users.noreply.github.com>
2021-04-07 17:13:57 -0700

    Added nectar and houses

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    int honey = 0;
+    int nectar = 0;
+    public TextMeshProUGUI honeyText;
+    public TextMeshProUGUI nectarText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+        transform.Rotate(Input.GetAxis("Vertical"),0.0f,-Input.GetAxis("Horizontal"));
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Hive"){
+            honey += 10;
+            honeyText.text = "Honey: " + honey.ToString();
+        }
+        if(other.gameObject.tag == "Planter"){
+            nectar += 10;
+            nectarText.text = "Nectar: " + nectar.ToString();
+        }
+    }
+}
